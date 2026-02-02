680c849317d2e0e8f9e68101262afd4a187f39ba
Shubham Barhate <shubhambarhate3063@gmail.com>
2026-02-02 16:36:25 +0530

    Course Controller Added

---
+++ IMS_Server/ims-backend/src/main/java/com/ims/controller/CourseController.java
@@ -0,0 +1,38 @@
+package com.ims.controller;
+
+import com.ims.entity.Course;
+import com.ims.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public Course createCourse(@RequestBody Course course) {
+        return courseService.saveCourse(course);
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+    }
+}
