7b127187090780b161e8b65ec9538b297a4bb546
Shubham Barhate <shubhambarhate3063@gmail.com>
2026-02-02 16:25:22 +0530

    Course and Exam service implementation added

---
+++ IMS_Server/ims-backend/src/main/java/com/ims/service/impl/ExamServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ims.service.impl;
+
+import com.ims.entity.Exam;
+import com.ims.repository.ExamRepository;
+import com.ims.service.ExamService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ExamServiceImpl implements ExamService {
+
+    private final ExamRepository examRepo;
+
+    public ExamServiceImpl(ExamRepository examRepo) {
+        this.examRepo = examRepo;
+    }
+
+    @Override
+    public Exam saveExam(Exam exam) {
+        return examRepo.save(exam);
+    }
+
+    @Override
+    public List<Exam> getAllExams() {
+        return examRepo.findAll();
+    }
+
+    @Override
+    public Exam getExamById(Long id) {
+        return examRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Exam not found with id: " + id));
+    }
+
+    @Override
+    public void deleteExam(Long id) {
+        examRepo.deleteById(id);
+    }
+}
