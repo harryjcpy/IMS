227b3cb8e6bb1ea3ba5cc3a006ff0f8b5495f325
TusharSonawne <sonawnetushar8@gmail.com>
2026-02-02 16:30:14 +0530

    Attendance Service added

---
+++ IMS_Server/ims-backend/src/main/java/com/ims/service/impl/AccountantServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ims.service.impl;
+
+import com.ims.entity.Accountant;
+import com.ims.repository.AccountantRepository;
+import com.ims.service.AccountantService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AccountantServiceImpl implements AccountantService {
+
+    private final AccountantRepository accountantRepo;
+
+    public AccountantServiceImpl(AccountantRepository accountantRepo) {
+        this.accountantRepo = accountantRepo;
+    }
+
+    @Override
+    public Accountant saveAccountant(Accountant accountant) {
+        return accountantRepo.save(accountant);
+    }
+
+    @Override
+    public List<Accountant> getAllAccountants() {
+        return accountantRepo.findAll();
+    }
+
+    @Override
+    public Accountant getAccountantById(Long id) {
+        return accountantRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Accountant not found with id: " + id));
+    }
+
+    @Override
+    public void deleteAccountant(Long id) {
+        accountantRepo.deleteById(id);
+    }
+}
