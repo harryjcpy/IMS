886e7874df3e96bb758759cc1d450eb9741d9d25
unknown <bhambure1994@gmail.com>
2026-02-02 15:41:20 +0530

    Result Repository added

---
+++ IMS_Server/ims-backend/src/main/java/com/ims/repository/ResultRepository.java
@@ -0,0 +1,9 @@
+package com.ims.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ims.entity.Result;
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+
+}
