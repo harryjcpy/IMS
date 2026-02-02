ee2dfabb681d5b536f5d074fae25daf08fb503c8
unknown <bhambure1994@gmail.com>
2026-02-02 16:20:25 +0530

    Teacher Controller added

---
+++ IMS_Server/ims-backend/src/main/java/com/ims/controller/TeacherController.java
@@ -0,0 +1,38 @@
+package com.ims.controller;
+
+import com.ims.entity.Teacher;
+import com.ims.service.TeacherService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teachers")
+public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping
+    public Teacher createTeacher(@RequestBody Teacher teacher) {
+        return teacherService.saveTeacher(teacher);
+    }
+
+    @GetMapping
+    public List<Teacher> getAllTeachers() {
+        return teacherService.getAllTeachers();
+    }
+
+    @GetMapping("/{id}")
+    public Teacher getTeacherById(@PathVariable Long id) {
+        return teacherService.getTeacherById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTeacher(@PathVariable Long id) {
+        teacherService.deleteTeacher(id);
+    }
+}
